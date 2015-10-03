a433a322876e70d6ddfa7c6d9755415034e101dc
Kamal Wood <kwood@deloitte.co.uk>
2015-10-03 19:52:16 +0100

    Changed api url. Added configuration to get media to work on heroic.

--- homely/urls.py
+++ homely/urls.py
@@ -9,7 +9,7 @@ router.register(r'receivers', ReceiverViewSet)
 router.register(r'donations', DonationViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api/', include(router.urls)),
+    url(r'^api/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
